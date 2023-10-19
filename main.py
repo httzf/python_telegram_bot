572b7020d50674afe38763b486afca63afe85af9
httzf <148317021+httzf@users.noreply.github.com>
2023-10-19 23:54:27 +0300

    Добавила диалог между учениками из каждого класса

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
-from Students import Student
+class Student:
+    """
+    Класс для представления студента в БД
+    """
+
+    def __init__(self, name, age, fav_subj, grade):
+        self.name = name
+        self.age = age
+        self.fav_subj = fav_subj
+        self.grade = grade
+        self.is_dumb_boy = False
+        self.cringe_boy = False
+        self.friendship = False
+        self.enemies = False
+
+    def says_hello(self):
+        print('Ты странный')
+
+    def hit(self, other):
+
+        if self.is_dumb_boy:
+            print(f'{self.name}, ты кринж')
+            self.enemies = True
+
+        if other.cringe_boy:
+            print(f'{self.name}, это шутка, бро')
+            other.friendship = True
+
+        self.is_dumb_boy = True
+        other.cringe_boy = True
 
 
-class Grade8(Student):
-    def __init__(self, name, age, fav_subj):
-        super().__init__(name, age, fav_subj, 8)
+student1 = Student('Ушак', 19, 'IT', 13)
+student2 = Student('Женя', 19, 'math', 13)
 
-
-class Grade9(Student):
-    def __init__(self, name, age, fav_subj):
-        super().__init__(name, age, fav_subj, 9)
-
-
-class Grade10(Student):
-    def __init__(self, name, age, fav_subj):
-        super().__init__(name, age, fav_subj, 10)
-
-
-class Grade11(Student):
-    def __init__(self, name, age, fav_subj):
-        super().__init__(name, age, fav_subj, 11)
-
+student1.says_hello()
+student2.hit(student1)
+student2.hit(student2)
+student1.hit(student1)
